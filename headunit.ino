0eef3f330080f02746f2757465c5d2ef3e91b76b
Chris Harper <nomath4u@gmail.com>
2016-09-21 18:18:30 -0700

    Now actually working. Also rewrote metadata gathering function

--- headunit.ino
+++ headunit.ino
@@ -4,6 +4,7 @@
 #include <Adafruit_ST7735.h>
 #include <SPI.h>
 #include <SD.h> //This needs to be the Adafruit one if not using arduino v23
+#include <string.h>
 
 
 /*Defines for the 1.8" TFT from adafruit working on a Teensy 3.1 
@@ -15,7 +16,7 @@ with hardware SPI in normal configuration*/
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
 //Text size information
-#define BASE_CHAR_HEIGHT 5
+#define BASE_CHAR_HEIGHT 8
 #define SIZE_MULT 1
 #define CHAR_SIZE (BASE_CHAR_HEIGHT * SIZE_MULT)
 #define TITLE_OFFSET 0
@@ -41,11 +42,12 @@ void setup(void) {
   
 
   Serial.println("Initialized");
-  if(sd_init()){
-    draw_background("background.bmp");
-  } else {
-    tft.fillScreen(ST7735_BLACK);
-  }
+  //if(sd_init()){
+  //  draw_background("background.bmp");
+ // } else {
+  //  tft.fillScreen(ST7735_BLACK);
+  //}
+  tft.fillScreen(ST7735_BLACK);
   delay(500);
 
   /*Once this is working try what is in the print test*/
@@ -59,11 +61,11 @@ void loop() {
   char str[STRLEN] = {'\0'};
   String data = "";
   //Clear out anything from before
-  while(Serial2.available()){
-    char c = Serial2.read();
-  }
-  Serial2.print("AD\r"); //Get metadata
-  delay(100); //Let the command be processed switch to a while loop waiting on available in the future
+  //while(Serial2.available()){
+  //  char c = Serial2.read();
+  //}
+  //Serial2.print("AD\r"); //Get metadata
+  //delay(100); //Let the command be processed switch to a while loop waiting on available in the future
   data = get_metadata(&i);
   artist = get_artist(data);
   album = get_album(data);
@@ -71,9 +73,10 @@ void loop() {
 
   tft.fillScreen(ST7735_BLACK);
 
-  draw_title(title.toCharArray(), title.length());
-  draw_artist(artist.toCharArray(), artist.length());
-  draw_album(album.toCharArray(), album.length());
+  draw_title(title.c_str(), ST7735_WHITE);
+  draw_artist(artist.c_str(), ST7735_WHITE);
+  draw_album(album.c_str(), ST7735_WHITE);
+  Serial.println(data);
   delay(3000); //Check for new song every 3 seconds
 }
 
@@ -82,12 +85,17 @@ String get_metadata(int* i){
   int complete = 0;
   int timer = 0;
   String data;
+  while(Serial2.available()){
+    char k = Serial2.read();
+  }
+  Serial2.print("AD\r");
+  while(!Serial2.available()); //Wait until there is stuff on the line
   while( complete < 7){
     if(Serial2.available()){
       char c = Serial2.read();
       data += c;
       timer = millis();
-      i++;
+      *i++;
       if(c == '\n'){
         complete++;
       }
@@ -100,40 +108,47 @@ String get_metadata(int* i){
   return data;
 }
 
+//while(Serial.available()){
+//  char c = read();
+//}
+
+
 String get_artist(String metadata){
-    i = metadata.indexOf("Artist=") + 7;
-    if (i != 1){
-      metadata.remove(0,n); //Removes everything up to the artist
+    int i = metadata.indexOf("Artist=") + 7;
+    if (i != 6){ // -1 + offset
+      metadata.remove(0,i); //Removes everything up to the artist
       i = metadata.indexOf('\n'); //Gets end of line after artist
       metadata.remove(i);
     } else {
       metadata = ""; //Don't know what it is
     }
-    return metadata
+    return metadata;
 }
 
 String get_title(String metadata){
-    i = metadata.indexOf("Title=") + 6;
-    if (i != 1){
-      metadata.remove(0,n); //Removes everything up to the artist
+    int i = metadata.indexOf("Title=") + 6;
+    if (i != 5){// -1 + offset
+      metadata.remove(0,i); //Removes everything up to the artist
       i = metadata.indexOf('\n'); //Gets end of line after artist
       metadata.remove(i);
     } else {
       metadata = ""; //Don't know what it is
     }
-    return metadata
+    return metadata;
 }
 
 String get_album(String metadata){
-    i = metadata.indexOf("Album=") + 6;
-    if (i != 1){
-      metadata.remove(0,n); //Removes everything up to the artist
+    int i = metadata.indexOf("Album=") + 6;
+    Serial.println("Index");
+    Serial.println(i);
+    if (i != 5){ //-1 + offset
+      metadata.remove(0,i); //Removes everything up to the artist
       i = metadata.indexOf('\n'); //Gets end of line after artist
       metadata.remove(i);
     } else {
       metadata = ""; //Don't know what it is
     }
-    return metadata
+    return metadata;
 }
 
 void testdrawtext(char *text, uint16_t color) {
@@ -143,8 +158,8 @@ void testdrawtext(char *text, uint16_t color) {
   tft.print(text);
 }
 
-void draw_title(char *text, uint16_t color) {
-  tft.setCursor((TITLE_OFFSET * CHAR_SIZE) + V_BUFF, 0);
+void draw_title(const char *text, uint16_t color) {
+  tft.setCursor(0, (TITLE_OFFSET * CHAR_SIZE) + V_BUFF);
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.setTextSize(SIZE_MULT);
@@ -152,8 +167,8 @@ void draw_title(char *text, uint16_t color) {
   tft.print(text);
 }
 
-void draw_artist(char *text, uint16_t color) {
-  tft.setCursor((ARTIST_OFFSET * CHAR_SIZE) + V_BUFF, 0);
+void draw_artist(const char *text, uint16_t color) {
+  tft.setCursor(0,(ARTIST_OFFSET * CHAR_SIZE) + V_BUFF);
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.setTextSize(SIZE_MULT);
@@ -161,12 +176,12 @@ void draw_artist(char *text, uint16_t color) {
   tft.print(text);
 }
 
-void draw_album(char *text, uint16_t color) {
-  tft.setCursor((ALBUM_OFFSET * CHAR_SIZE) + V_BUFF, 0);
+void draw_album(const char *text, uint16_t color) {
+  tft.setCursor(0,(ALBUM_OFFSET * CHAR_SIZE) + V_BUFF);
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.setTextSize(SIZE_MULT);
-  tft.print("Artist: ");
+  tft.print("Album: ");
   tft.print(text);
 }
 
@@ -181,6 +196,6 @@ int sd_init(){
 }
 
 void draw_background(char* file_name){
-  bmpDraw(file_name, 0,0);
+  //tft.bmpDraw(file_name, 0,0);
   delay(5000); //Wait 5 seconds for it to render
   }
